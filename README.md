0426670d1c72739ff049cc2447ef9cbedec7d2a8
Zoten64 <forjavligratta@gmail.com>
2024-04-18 17:33:45 +0200

    Feat: Connect database and change secret key
    - Install dependencies: dj-database-url and psycopg2
    - Add URL to database in env.py file
    - Connect database in theharborhearth/settings.py
    - Move secret key to env.py

--- README.md
+++ README.md
@@ -21,5 +21,10 @@
 The goal of this project is to put my knowledge of HTML, CSS, JS, Python, Django and relational databases as well 
 as push myself to learn more.
 
-The Harbor Hearth is a fictional bistro located at the west coast of England. They offer online ordering and a digital menu. Clients can
-review the bakery, create an account to be recieve loyalty points that can be exchanged for goods. Ordering online you may also get deals.
+The Harbor Hearth is a fictional bistro nearby a vacation resort located at the west coast of England. They offer online ordering and a digital menu. Clients can
+review the bakery, create an account to be recieve loyalty points with every order that can be exchanged for goods. Making an account will also let you take part of deals
+and save your payment information for faster ordering. Though an account is not required for ordering as that may be frustrating for users.
+
+The website also features a blog where the admins can post news, changes and other relevant information. Customers may comment and ask questions.
+
+As an admin you can create special accounts for employees where they have access to a page with pending orders with the ability to cancel. They also have access to answer and approve questions as well as delete inappropriate ones. 
--- theharborhearth/settings.py
+++ theharborhearth/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+
+
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kypt5kfa4$&c(kc$#6+^@59d7y_fkk0nt&(mk&#8$#x+5p-z79'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -73,11 +79,15 @@ WSGI_APPLICATION = 'theharborhearth.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
