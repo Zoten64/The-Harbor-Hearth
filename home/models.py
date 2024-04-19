fbfaf34431344aed1f645ace439e5d359f8d2a66
Zoten64 <forjavligratta@gmail.com>
2024-04-20 01:51:35 +0200

    Feat: Add model for menu items
    - Make the model for menu items
    - Add the model to the admin panel

---
+++ home/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+# Create your models here.
+
+MENU_CATEGORY = (("Meal", "MEAL"), ("Drink", "DRINK"), ("Dessert", "DESSERT"))
+
+class Menu(models.Model):
+    '''
+    Model to add items to the menu
+    '''
+
+    category = models.CharField(choices=MENU_CATEGORY, default=0)
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    ingridients = models.TextField()
+    price = models.IntegerField()
+    vegan = models.BooleanField(default=False)
+    nuts = models.BooleanField(default=False)
+    lactose = models.BooleanField(default=False)
+    gluten = models.BooleanField(default=False)
+    eggs = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["category"]
+
+    def __str__(self):
+        return f"{self.category} | {self.name}"
+
