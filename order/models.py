60bde1cf9176d06a92678b219bcd2b1ff6dc77dc
Zoten64 <forjavligratta@gmail.com>
2024-04-21 23:43:42 +0200

    Feat: Add delivery method field to order
    - Add delivery choices to orders (eat-in: counter, eat-in: table, take-out)
    - Add field to placeholder order view template

--- order/models.py
+++ order/models.py
@@ -1,11 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+DELIVERY_METHOD = (("Eat-in: Counter", "EAT-IN-COUNTER"), 
+                   ("Eat-in: Table", "EAT-IN-TABLE"), ("Take-Out", "TAKE-OUT"))
+
 class Order(models.Model):
     '''
     Model for adding orders to the database
     '''
     order_number = models.AutoField(primary_key=True)
+    delivery_method = models.CharField(choices=DELIVERY_METHOD, 
+                                       default=0)
     order = models.TextField()
     finished = models.BooleanField(default=False)
     date = models.DateTimeField(auto_now_add=True)
