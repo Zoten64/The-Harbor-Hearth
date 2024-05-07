6a34f55bdd9861d35a3cbbc9684d3662c184e4db
Zoten64 <forjavligratta@gmail.com>
2024-05-07 22:44:23 +0200

    Fix: Fix non approved reviews showing up
    - Change view from class based to function based
    - Rename reviews_list.html to review.html

--- review/views.py
+++ review/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Review
 
 # Create your views here.
-class Review(generic.ListView):
-    model = Review
+def ReviewList(request):
+
+    reviews = Review.objects.filter(approved=True) 
+    context = {
+        'reviews' : reviews,
+    }
+    return render(request, 'review/review.html', context)
