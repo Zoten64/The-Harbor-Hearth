2ab088081be090daed888d25c4cebe367673444e
Zoten64 <forjavligratta@gmail.com>
2024-05-09 03:38:51 +0200

    Feat: Add read more review functionality
    - Add review/review_detail.html
    - reorder css imports so that tailwind is above output.css for priority reasons

--- review/urls.py
+++ review/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.ReviewList.as_view(), name='review'),
+    path('<uuid:url>/', views.ReviewDetail, name='review_detail')
 ]
--- review/views.py
+++ review/views.py
@@ -7,3 +7,13 @@ class ReviewList(generic.ListView):
     queryset = Review.objects.filter(approved=True)
     template_name = "review/review_list.html"
     paginate_by = 6
+
+def ReviewDetail(request, url):
+    reviews = Review.objects.filter(approved=True)
+    review = get_object_or_404(reviews, url=url)
+
+    context = {
+        'review' : review
+    }
+
+    return render(request, 'review/review_detail.html', context)
