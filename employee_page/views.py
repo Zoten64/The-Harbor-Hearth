c08a4a78033236d74a8a6366bae636f25232b30f
Zoten64 <forjavligratta@gmail.com>
2024-05-19 18:41:20 +0200

    Fix: Make employee pages only available to registered employee accounts
    - Add basic 403 template

--- employee_page/views.py
+++ employee_page/views.py
@@ -6,86 +6,101 @@ from contact.models import ContactForm
 from .forms import Cancel, ChangeStatus
 from django.contrib import messages
 from django.core.mail import send_mail
+from django.core.exceptions import PermissionDenied
 
 
 # Create your views here.
 
+def EmployeeAuth(request):
+    user = request.user
+    try:
+        employee = Employee.objects.filter(user=user)
+    except:
+        raise PermissionDenied()
+    if not employee:
+        raise PermissionDenied()
+    else:
+        return True
 
 def EmployeeHome(request):
-    context = {}
-    user = request.user
-    employee = Employee.objects.filter(user=user)
-    print(employee)
-    return render(request, 'employee_page/employee_home.html', context)
+    if EmployeeAuth(request):
+        context = {}
+        return render(request, 'employee_page/employee_home.html', context)
 
 
 def EmployeeOrders(request):
-    unfinished_orders = Order.objects.filter(state="Not Started")
-    in_progress_orders = Order.objects.filter(state="In progress")
-    finished_orders = Order.objects.filter(state="Finished")
+    if EmployeeAuth(request):
+        unfinished_orders = Order.objects.filter(state="Not Started")
+        in_progress_orders = Order.objects.filter(state="In progress")
+        finished_orders = Order.objects.filter(state="Finished")
 
-    context = {"unfinished_orders": unfinished_orders,
-               "in_progress_orders": in_progress_orders,
-               "finished_orders": finished_orders}
+        context = {"unfinished_orders": unfinished_orders,
+                "in_progress_orders": in_progress_orders,
+                "finished_orders": finished_orders}
 
-    return render(request, 'employee_page/employee_orders.html', context)
+        return render(request, 'employee_page/employee_orders.html', context)
 
 
 def EmployeeOrderDetail(request, order_number):
-    order = Order.objects.all()
-    order = get_object_or_404(order, order_number=order_number)
+    if EmployeeAuth(request):
+        order = Order.objects.all()
+        order = get_object_or_404(order, order_number=order_number)
 
-    if request.method == 'POST':
-        new_status = ChangeStatus(request.POST)
-        if new_status.is_valid():
-            order.state = request.POST['state']
-            order.save()
-            messages.success(request, 'Status change successful')
+        if request.method == 'POST':
+            new_status = ChangeStatus(request.POST)
+            if new_status.is_valid():
+                order.state = request.POST['state']
+                order.save()
+                messages.success(request, 'Status change successful')
 
-    form = ChangeStatus
+        form = ChangeStatus
 
-    context = {
-        'order': order,
-        'form' : form
-    }
+        context = {
+            'order': order,
+            'form' : form
+        }
 
-    return render(request, 'employee_page/employee_order_detail.html', context)
+        return render(request, 'employee_page/employee_order_detail.html', 
+                      context)
 
 
 def EmployeeContactForms(request):
-    contact_forms = ContactForm.objects.all()
-    context = {"contact_forms": contact_forms}
+    if EmployeeAuth(request):
+        contact_forms = ContactForm.objects.all()
+        context = {"contact_forms": contact_forms}
 
-    return render(request, 'employee_page/employee_contact_forms.html', context)
+        return render(request, 'employee_page/employee_contact_forms.html', 
+                      context)
 
 
 def EmployeeCancelOrderConfirm(request, order_number):
-    order = Order.objects.all()
-    order = get_object_or_404(order, order_number=order_number)
+    if EmployeeAuth(request):
+        order = Order.objects.all()
+        order = get_object_or_404(order, order_number=order_number)
 
-    if request.method == 'POST':
-        cancel_reason = Cancel(request.POST)
-        if cancel_reason.is_valid():
-            order.state = 'cancelled'
-            order.cancel_reason = request.POST['cancel_reason']
-            order.save()
+        if request.method == 'POST':
+            cancel_reason = Cancel(request.POST)
+            if cancel_reason.is_valid():
+                order.state = 'cancelled'
+                order.cancel_reason = request.POST['cancel_reason']
+                order.save()
 
-            message = "Your order was cancelled for the following reason: \n" \
-                        f"{order.cancel_reason}"
-            send_mail(
-                "Your order was cancelled",
-                message,
-                None,
-                [order.email],
-                fail_silently=False,
-            )
-            messages.success(request, 'Cancellation successful')
+                message = "Your order was cancelled for the following reason:" \
+                            f"\n{order.cancel_reason}"
+                send_mail(
+                    "Your order was cancelled",
+                    message,
+                    None,
+                    [order.email],
+                    fail_silently=False,
+                )
+                messages.success(request, 'Cancellation successful')
 
-    form = Cancel
+        form = Cancel
 
-    context = {
-        'order': order,
-        'form': form
-    }
-    return render(request, "employee_page/employee_cancel_order.html",
-                  context)
+        context = {
+            'order': order,
+            'form': form
+        }
+        return render(request, "employee_page/employee_cancel_order.html",
+                    context)
