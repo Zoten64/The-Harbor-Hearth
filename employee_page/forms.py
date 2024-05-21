59637176d08e8e95f74bdeee1ece58407766f16a
Zoten64 <forjavligratta@gmail.com>
2024-05-21 19:20:37 +0200

    Style: Style the cancel order page - fix the input text blox being the same color as the background in darkmode

--- employee_page/forms.py
+++ employee_page/forms.py
@@ -4,20 +4,23 @@ from order.models import Order
 STATE = (("Not Started", "Not Started"), ("In progress", "in progress"),
          ("Finished", "finished"))
 
+
 class Cancel(forms.Form):
-    cancel_reason = forms.CharField(label="Cancel reason", max_length=200)
+    cancel_reason = forms.CharField(widget=forms.TextInput(
+        attrs={"class": "field_text_input"}), label="Cancel reason", 
+        max_length=200)
 
     class Meta:
         fields = ('cancel_reason',)
-        #This is the association between the model and the model form
+        # This is the association between the model and the model form
         model = Order
 
 
 class ChangeStatus(forms.Form):
-    state = forms.ChoiceField(widget=forms.RadioSelect(attrs={"class" : "my-2"}), 
+    state = forms.ChoiceField(widget=forms.RadioSelect(attrs={"class": "my-2"}),
                               choices=STATE, label='')
 
     class Meta:
         fields = ('state',)
-        #This is the association between the model and the model form
+        # This is the association between the model and the model form
         model = Order
