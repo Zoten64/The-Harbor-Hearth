1429013529cca98ed46f8aa5edb18262e0264f05
Zoten64 <forjavligratta@gmail.com>
2024-05-22 22:14:16 +0200

    Feat: Add emailfield to order page if user is not logged in

---
+++ order/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from .models import Order
+
+DELIVERY_METHOD = (("Eat-in: Counter", "EAT-IN-COUNTER"), 
+                   ("Eat-in: Table delivery", "EAT-IN-TABLE"), 
+                   ("Take-Out", "TAKE-OUT"))
+
+# if the user is not logged in they will need to enter their email
+class OrderEmail(forms.Form):
+    email = forms.EmailField()
+
+    class Meta:
+        fields = ('email',)
+        # This is the association between the model and the model form
+        model = Order
+    
+class Order(forms.Form):
+    order = forms.Textarea()
+    delivery_method = forms.ChoiceField(choices=DELIVERY_METHOD)
+
+    class Meta:
+        fields = ('order', 'delivery_method')
+        # This is the association between the model and the model form
+        model = Order
--- order/views.py
+++ order/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 from django.views import generic
+from django.contrib.auth.models import User
 from .models import Order
+from .forms import OrderEmail, Order
 
 # Create your views here.
 def Order(request):
+
+    if request.user.is_authenticated:
+        status = 'logged in'
+        # if the user is logged in the email field will be automatically filled
+        # out in the post
+        email_form = ''
+    else:
+        # if the user is not logged in they'll have to enter their email
+        email_form = OrderEmail
+
+    order_form = Order
     context = {
+        'email' : email_form,
+        'order_form' : order_form
     }
+
     return render(request, 'order/order.html', context)
